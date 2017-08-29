a21fcbceda73206616bd61ed1a3bd5cdf41248bb
Tony MACK <tmack@e5r9p2.wethinkcode.co.za>
2017-08-29 18:27:19 +0200

    test

--- Bomberman/src/block.cpp
+++
@@ -1,9 +0,0 @@
-#include <block.hpp>
-
-Block::Block(int id, std::string type, bool breakable) : NonStatic(id, type), _breakable(breakable)
-{}
-
-void Block::setPosition(float x, float y, float z)
-{
-	translate(glm::vec3(x, y, z));
-}
--- Core/includes/zion.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-#include <window.hpp>
-#include <util.h>
-#include <shader.hpp>
-#include <staticSprite.hpp>
-#include <camera.hpp>
-#include <gltf.hpp>
